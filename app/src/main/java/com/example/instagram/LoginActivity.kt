15333a00e86841f64701eb1b6963dfafa9aba3ef
Leesungsup <tmdtjq0116@naver.com>
2022-01-13 03:08:27 +0900

    google firebase

--- app/src/main/java/com/example/instagram/LoginActivity.kt
+++ app/src/main/java/com/example/instagram/LoginActivity.kt
@@ -1,16 +1,28 @@
 package com.example.instagram
 
+import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     var auth : FirebaseAuth?=null
+    var googleSignInClient : GoogleSignInClient?=null
+    var GOOGLE_LOGIN_CODE=9001
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
@@ -18,6 +30,57 @@ class LoginActivity : AppCompatActivity() {
         email_login_button.setOnClickListener{
             signinAndSignup()
         }
+        google_sign_in_button.setOnClickListener{
+
+            googleLogin()
+        }
+        var gso=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient=GoogleSignIn.getClient(this,gso)
+    }
+    fun googleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+
+        startForResult.launch(signInIntent)
+    }
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result: ActivityResult ->
+        if(result.resultCode== GOOGLE_LOGIN_CODE) {
+            val intent:Intent=result.data!!
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(intent)
+            try{
+                val account =task.getResult(ApiException::class.java)!!
+                Log.d(ContentValues.TAG,"firebaseAuthWithGoogle"+account.id)
+                firebaseAuthWithGoogle(account)
+            }catch(e:ApiException){
+                Log.w(ContentValues.TAG,"Google sign in failed",e)
+            }
+        }
+    }
+    /*override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==GOOGLE_LOGIN_CODE){
+            var result= data?.let { Auth.GoogleSignInApi.getSignInResultFromIntent(it) }
+            if (result != null) {
+                if(result.isSuccess){
+                    var account=result.signInAccount
+                    firebaseAuthWithGoogle(account)
+                }
+            }
+        }
+    }*/
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount){
+        var credential=GoogleAuthProvider.getCredential(account.idToken!!,null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    moveMainPage(task.result?.user)
+                } else {
+                    Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
+                }
+            }
     }
     fun signinAndSignup(){
 
