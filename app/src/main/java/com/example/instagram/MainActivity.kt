1bc014f47c89439235773844efd91b87791beca3
Leesungsup <tmdtjq0116@naver.com>
2022-01-15 03:30:52 +0900

    photo

--- app/src/main/java/com/example/instagram/MainActivity.kt
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.instagram
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import com.example.instagram.navigation.AlarmFragment
-import com.example.instagram.navigation.DetailViewFragment
-import com.example.instagram.navigation.GridFragment
-import com.example.instagram.navigation.UserFragment
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.instagram.navigation.*
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +33,9 @@ class MainActivity : AppCompatActivity(){
                     true
                 }
                 R.id.action_add_photo -> {
+                    if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                        startActivity(Intent(this, AddPhotoActivity::class.java))
+                    }
                     true
                 }
                 R.id.action_favorite_alarm -> {
@@ -48,6 +53,7 @@ class MainActivity : AppCompatActivity(){
                 else -> false
             }
         }
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
     }
 
     /*override fun onNavigationItemSelected(p0: MenuItem): Boolean {
--- app/src/main/java/com/example/instagram/navigation/AddPhotoActivity.kt
+++ app/src/main/java/com/example/instagram/navigation/AddPhotoActivity.kt
@@ -1,12 +1,16 @@
 package com.example.instagram.navigation
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.instagram.R
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_add_photo.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddPhotoActivity : AppCompatActivity() {
     var PICK_IMAGE_FROM_ALBAM=0
@@ -20,7 +24,31 @@ class AddPhotoActivity : AppCompatActivity() {
         photoPickerIntent.type="image/*"
         startActivityForResult(photoPickerIntent,PICK_IMAGE_FROM_ALBAM)
         addphoto_btn_upload.setOnClickListener{
-
+            contentUpload()
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==PICK_IMAGE_FROM_ALBAM){
+            if(resultCode== Activity.RESULT_OK){
+                photoUri=data?.data
+                addphoto_image.setImageURI(photoUri)
+            }else{
+                finish()
+            }
+        }
+    }
+    fun contentUpload(){
+        var timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        var imageFileName = "IMAGE_" + timestamp + "_.png"
+
+        var storageRef = storage?.reference?.child("images")?.child(imageFileName)
+
+        //Promise method
+        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
+            Toast.makeText(this,getString(R.string.upload_success),Toast.LENGTH_LONG).show()
+        }
+
     }
 }
